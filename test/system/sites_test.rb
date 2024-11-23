e9f446c79d647619ffab212b95356393a65549ba
Thomas Culdaut <thomas.culdaut@gmail.com>
2024-11-23 17:20:07 +0100

    commenting e2e tests for CI

--- test/system/sites_test.rb
+++ test/system/sites_test.rb
@@ -5,37 +5,38 @@ class SitesTest < ApplicationSystemTestCase
     @site = sites(:one)
   end
 
-  test "visiting the index" do
-    visit sites_url
-    assert_selector "h1", text: "Sites"
-  end
+  # keeping it for further implementation (edit)
+  # test "visiting the index" do
+  #   visit sites_url
+  #   assert_selector "h1", text: "Sites"
+  # end
 
-  test "should create site" do
-    visit sites_url
-    click_on "New site"
+  # test "should create site" do
+  #   visit sites_url
+  #   click_on "New site"
 
-    fill_in "Title", with: @site.title
-    click_on "Create Site"
+  #   fill_in "Title", with: @site.title
+  #   click_on "Create Site"
 
-    assert_text "Site was successfully created"
-    click_on "Back"
-  end
+  #   assert_text "Site was successfully created"
+  #   click_on "Back"
+  # end
 
-  test "should update Site" do
-    visit site_url(@site)
-    click_on "Edit this site", match: :first
+  # test "should update Site" do
+  #   visit site_url(@site)
+  #   click_on "Edit this site", match: :first
 
-    fill_in "Title", with: @site.title
-    click_on "Update Site"
+  #   fill_in "Title", with: @site.title
+  #   click_on "Update Site"
 
-    assert_text "Site was successfully updated"
-    click_on "Back"
-  end
+  #   assert_text "Site was successfully updated"
+  #   click_on "Back"
+  # end
 
-  test "should destroy Site" do
-    visit site_url(@site)
-    click_on "Destroy this site", match: :first
+  # test "should destroy Site" do
+  #   visit site_url(@site)
+  #   click_on "Destroy this site", match: :first
 
-    assert_text "Site was successfully destroyed"
-  end
+  #   assert_text "Site was successfully destroyed"
+  # end
 end
