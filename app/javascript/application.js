c97131d410c46b6cd97ced1c88d0ae2b23cba726
Thomas Culdaut <thomas.culdaut@gmail.com>
2024-11-30 17:11:56 +0100

    wip gallery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,9 @@ import morphdom from "morphdom"
 import "controllers"
 
 document.addEventListener("turbo:before-frame-render", (event) => {
+  const originalRender = event.detail.render;
     event.detail.render = (currentElement, newElement) => {
-      morphdom(currentElement, newElement, { childrenOnly: true })
+      document.startViewTransition(() => originalRender(currentElement, newElement));
+      // morphdom(currentElement, newElement, { childrenOnly: true })
     }
   })
